003d028b78b83a9a7884bb937a4a1cee40d86974
Rubina Rekić <rrekic1@etf.unsa.ba>
2026-05-11 20:00:18 +0200

    trigger backend deployment

--- PROJEKAT/backend/src/PostRoute.Api/Configuration/ApiServiceRegistration.cs
+++ PROJEKAT/backend/src/PostRoute.Api/Configuration/ApiServiceRegistration.cs
@@ -7,6 +7,7 @@ public static class ApiServiceRegistration
 {
     public static IServiceCollection AddApiLayer(this IServiceCollection services, IConfiguration configuration)
     {
+        //trigger backend check
         services.AddControllers();
         services.AddOpenApi();
         services.AddBusinessLayer(configuration);
